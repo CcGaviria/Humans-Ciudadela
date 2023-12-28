9366b75c823ef58c90c888ad970349be7e09b25f
Cristian Gaviria <Cristian@Gaviria.Org>
2023-12-28 18:25:53 -0500

    Add Types

---
+++ client-species-nextjs/src/types/index.tsx
@@ -0,0 +1,4 @@
+type OriginType = { name: String };
+type LocationType = { name: String };
+type CharacterType = { name: String, gender: String, status: String, origin: OriginType, location: LocationType, image: string };
+type ItemCardEspecieType = {title: String, linkTo:string, description:String}
