adbfccb8dc2e53d5cf61e39beb85635daf5331c9
Cristian Gaviria <Cristian@Gaviria.Org>
2023-12-28 18:05:28 -0500

    Create cloudfront, s3 resources
    Project to guarantee the continuity of services in the event of a failure

---
+++ infrastructure-species-tf/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+
+variable "access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
